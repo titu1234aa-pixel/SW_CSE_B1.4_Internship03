82fd380ead716136365af1c6c6aaed44c5141563
titu1234aa-pixel <titu1234aa@gmail.comgit config --global user.name titu1234aa-pixelgit config --global user.email titu1234aa@gmail.com>
2026-07-28 21:18:09 +0530

    var: function scoped

--- day8/j4.js
+++ day8/j4.js
@@ -3,19 +3,46 @@ var x = 10;
 var x = 20;
 x = 30;
 console.log(x)
+// console.log(x)
 
 if (true) {
     var message = "Hello";
 }
 // var in JS is not block-scoped
 console.log(message);
+// console.log(message);
+
+// ****************************************************
+// let keyword based variable
 let score = 100;
+//let score = 100; //SyntaxError: Identifier 'score' has already been declared
 score = 160; // re-assignment is valid
 console.log(score)
+// console.log(score)
+
 if(true){
     let secret = "Password@123"; // let in JS is block-scoped
+@@ -28,4 +28,22 @@ if(true){
+    //pi = 27 //TypeError: Assignment to constant variable.
 }
-console.log(secret); 
-if(true){
-    const pi = 3.14159;
-    console.log(pi)
+// const in JS is block-scoped //ReferenceError: pi is not defined
+console.log(pi) 
+// console.log(pi) 
+
+
+// var: function scoped | Not recommended 
+// let & const: block-scoped
+
+// var: function scoped
+function test(){
+    if(true){
+        var a = 10;
+        let b = 20;
+        const c = 30;
+    }
+    // console.log(a); //worked
+    // console.log(b); //not worked
+    // console.log(c); //not worked
+}
+// console.log(a);
+test()
