1789de743375ad81eb8edc204b4aa0dc6d810971
titu1234aa-pixel <titu1234aa@gmail.comgit config --global user.name titu1234aa-pixelgit config --global user.email titu1234aa@gmail.com>
2026-07-28 21:14:22 +0530

    let keyword based variable

--- day8/j4.js
+++ day8/j4.js
@@ -9,3 +9,10 @@ if (true) {
 }
 // var in JS is not block-scoped
 console.log(message);
+let score = 100;
+score = 160; // re-assignment is valid
+console.log(score)
+if(true){
+    let secret = "Password@123"; // let in JS is block-scoped
+}
+console.log(secret); 
