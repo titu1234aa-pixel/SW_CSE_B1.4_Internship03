a317875981f088f04dabe71e390bb10a6e5b8700
titu1234aa-pixel <titu1234aa@gmail.comgit config --global user.name titu1234aa-pixelgit config --global user.email titu1234aa@gmail.com>
2026-08-08 16:56:21 +0530

    Props: Ex2 children: content passed between opening and closing tags, default props, css styling

--- day13/day13/src/App.jsx
+++ day13/day13/src/App.jsx
@@ -1,37 +1,45 @@
-import Button from './components/FunctionalComponent'
-import { LeftArrow,RightArrow } from "./components/FunctionalComponentTwo";
-
+```jsx
+import Button from "./components/FunctionalComponent";
+import { LeftArrow, RightArrow } from "./components/FunctionalComponentTwo";
 import ProfileList from "./components/Props";
+
 const name = "Amay";
 const isLoggedIn = false;
 
 function App() {
-
   return (
-    // React Fragments
     <>
       {/* JSX Rule 1: Return one parent element */}
-      <h1>Hello</h1>
-      <p>Hello again</p>
+      <h2>Hello</h2>
+      <h2>Hello again</h2>
 
       {/* JSX Rule 2: Every tag must be closed properly */}
       <p>Error</p>
       <input type="text" />
 
-      {/* JSX Rule 3: use camelCase for attributes */}
-      <button onClick onSubmit>Click</button>
+      {/* JSX Rule 3: Use camelCase for attributes */}
+      <button onClick={() => alert("Clicked")} onMouseOver={() => console.log("Mouse over")}>
+        Click
+      </button>
 
       {/* JSX Rule 4: Embedding JavaScript with curly braces */}
       <h3>Hello, {name}</h3>
 
-      {/* JSX Rule 5: use expressions, not statements */}
+      {/* JSX Rule 5: Use expressions, not statements */}
       <p>{isLoggedIn ? "Welcome back" : "Please Login"}</p>
 
+      {/* Functional Component */}
       <Button />
+
+      {/* Functional Components with named exports */}
       <LeftArrow />
       <RightArrow />
 
+      {/* Props Component */}
       <ProfileList />
     </>
+  );
+}
 
-  )
+export default App;
+```
