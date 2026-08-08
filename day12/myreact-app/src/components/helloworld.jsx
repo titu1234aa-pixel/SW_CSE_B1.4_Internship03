45d7c0e3d57d9c610582037f721853f15b31ebdd
titu1234aa-pixel <titu1234aa@gmail.comgit config --global user.name titu1234aa-pixelgit config --global user.email titu1234aa@gmail.com>
2026-08-08 14:24:22 +0530

    HelloWorld.jsx: Creating a component App.jsx: Importing the component and rendered it

---
+++ day12/myreact-app/src/components/helloworld.jsx
@@ -0,0 +1,13 @@
+
+
+import { HelloWorld } from "./components/HelloWorld";
+   function App() {
+     return (
+        <div>
+            <h1>Hello, World!</h1>
+             <h3>Welcome to my first React App.</h3>
+              <HelloWorld />
+        </div>
+    )
+}
+
