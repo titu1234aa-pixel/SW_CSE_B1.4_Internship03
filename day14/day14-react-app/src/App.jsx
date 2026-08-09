df7f96afee0d0e1632392dcf8a836c9012abadd8
titu1234aa-pixel <titu1234aa@gmail.comgit config --global user.name titu1234aa-pixelgit config --global user.email titu1234aa@gmail.com>
2026-08-09 16:20:18 +0530

    Form ex1

--- day14/day14-react-app/src/App.jsx
+++ day14/day14-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
 import ThemeProviderDemo from "./components/HookThree";
+import SimpleNameForm from "./components/FormOne";
 
 function App() {
 
@@ -15,9 +16,10 @@ return (
       <ProductListDemo />
 
       <CounterHook/> */}
-      <UserListHook/>
       {/* <UserListHook/> */}
       <ThemeProviderDemo />
+      {/* <ThemeProviderDemo /> */}
+      <SimpleNameForm />
     </>
   )
 }
